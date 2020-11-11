8a8cda0fad4472d5811a22f4281c58b17004fa58
Estremo13 <63829314+Estremo13@users.noreply.github.com>
2020-11-11 01:13:28 -0600

    Add files via upload

---
+++ data/Primeryltimacampaadealfabetizacin_2.js
@@ -0,0 +1,9 @@
+var json_Primeryltimacampaadealfabetizacin_2 = {
+"type": "FeatureCollection",
+"name": "Primeryltimacampaadealfabetizacin_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Title": "043", "Path": "C:\/Documentos de Estremo\/Mapas\/Madrid\/Fotos\\300_043.jpg", "RelPath": ".\/Fotos\/300_043.jpg", "_ESTADO": "Puebla", "_MUNICIPIO": "Zihuateutla", "_COMUNIDAD": "Mazacoatlán", "_Cantidad": 2.0, "_Año": "2018 a 2019" }, "geometry": { "type": "Point", "coordinates": [ -97.923161, 20.223381 ] } },
+{ "type": "Feature", "properties": { "Title": "053", "Path": "C:\/Documentos de Estremo\/Mapas\/Madrid\/Fotos\\053.png", "RelPath": ".\/Fotos\/053.png", "_ESTADO": "Michoacán", "_MUNICIPIO": "Huiramba", "_COMUNIDAD": "El Sobrado, El Pedregal, Las Trojes, El Carmen y Las Tablas", "_Cantidad": 1.0, "_Año": "1985" }, "geometry": { "type": "Point", "coordinates": [ -101.462778, 19.514444 ] } }
+]
+}
